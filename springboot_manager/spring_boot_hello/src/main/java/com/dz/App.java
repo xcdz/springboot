164e26b59e5f709dbe3f6ecfe2177e3807ddf84d
dz <820057663@qq.com>
2019-06-05 14:50:47 +0800

    springboot整合

---
+++ springboot_manager/spring_boot_hello/src/main/java/com/dz/App.java
@@ -0,0 +1,18 @@
+package com.dz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // Spring应用启动起来
+        SpringApplication.run(App.class,args);
+    }
+}
